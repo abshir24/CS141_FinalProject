65e44a0f59e2b54ac7edcb2d9c279a699b6e8e23
Abshir <abshirm@live.com>
2018-06-04 13:04:37 -0700

    login and registration complete

--- Program.java
+++ Program.java
@@ -27,6 +27,22 @@ public class Program {
 		String answer = userinput.next().toLowerCase();
 		
 		if(answer.charAt(0) == 'y') {
+			String username = promptUserNameLogin(userinput,0);
+			
+			if(username.contains("Error")) System.out.println(username);
+			
+			else {
+				
+				String password = promptPasswordLogin(userinput,username);
+				
+				if(password.contains("Error")) System.out.println(password);
+				
+				else {
+					System.out.println("Login was successful!");
+					User loggedInUser = retrieveUserFromDb(username);
+				}
+				
+			}
 			
 		}
 		
@@ -37,45 +53,25 @@ public class Program {
 			
 			String username = promptUserName(userinput);
 			
-			System.out.println("Type a password (no spaces)");
+			String password = promptPassword(userinput);
 			
-			answer = userinput.next().toLowerCase();
-			
-			String password = answer;
-			
-			System.out.println("Please type choose your favorite genre's from below, if you would like to select all please type 'all', if not just choose by number(please place spaces between numbers)");
-			
-			System.out.println("1)Action 2)Romance 3)Horror 4)Childrens");
-			
-			String genre = userinput.next();
-			
-			if(genre.equals("all")) genres = new int[] {1,2,3,4};
-			
-			else {
-				Scanner line = new Scanner(genre);
-				
-				int num = line.nextInt();
-				
-				genres = numToArray(num);
-		
-				reverseArr(genres);
-			}
+			genres = promptGenre(userinput);
 			
 			String[] params = new String[] {username,password,Arrays.toString(genres),Arrays.toString(favs),Arrays.toString(recents)};
 			
 			createUserInDb(file,params);
 		
-			User user = new User(username,password,genres,favs,recents);	
+			User loggedInUser = new User(username,password,genres,favs,recents);	
 			
-			allUsers[allUsers.length-1] = user;
+			allUsers[allUsers.length-1] = loggedInUser;
 			
 			System.out.println("Program over");
-			
 			
 		}
 		
 	}
 	
+	//Writes user content to our database file
 	public static void createUserInDb(File file, String[]fields)throws FileNotFoundException
 	{
 		try{
@@ -96,6 +92,7 @@ public class Program {
 		  }
 	}
 	
+	//reverses array.
 	public static void reverseArr(int[]arr)
 	{
 		int s = 0, e = arr.length-1,temp;
@@ -108,6 +105,7 @@ public class Program {
 		
 	}
 	
+	//converts an integer into an array given 123 ==> {1,2,3}
 	public static int[] numToArray(int num)
 	{
 		int temp = num,idx = 0;
@@ -132,6 +130,8 @@ public class Program {
 		
 		return genres;
 	}
+	
+	//retrieves all users from the db and stores them into an array.
 	
 	public static User[] retrieveUsers(Scanner file,User[] allUsers)
 	{
@@ -206,7 +206,7 @@ public class Program {
 		return allUsers;
 	}
 	
-	
+	//counts the number of users in the DB each user has 5 fields associated with them
 	public static int countUsersInDB(Scanner file)
 	{
 		int user=0, totalUsers = 0;
@@ -231,7 +231,7 @@ public class Program {
 		
 	}
 	
-	
+	//removes brackets from string to make the process of converting string to array much easier
 	public static String removeBrackets(String str)
 	{
 		String returnStr = "";
@@ -266,6 +266,7 @@ public class Program {
 				
 	}
 
+	//Counts the number of integers in a string.
 	public static int countNums(String genreArrayString)
 	{
 		Scanner line = new Scanner(genreArrayString);
@@ -282,6 +283,7 @@ public class Program {
 		
 	}
 
+	//retrieves integers from a string and converts them to an integer array list.
 	public static ArrayList<Integer> stringToIntArray(String genreArrayString,int idx)
 	{
 		Scanner line = new Scanner(genreArrayString);
@@ -300,6 +302,7 @@ public class Program {
 		return returnArray;
 	}
 	
+	//Copies the elements from array list into array.
 	public static int[] copyList(ArrayList<Integer>list)
 	{
 		int size = list.size(); 
@@ -312,30 +315,153 @@ public class Program {
 		return returnArray;
 	}
 	
-	public static boolean findUserInDb(String username)
+	//Checks to see if user exists in DB
+	public static boolean isExistingUser(String username)
 	{
-		///problem is here!!!!!!
-		for(int i = 0;i<allUsers.length;i++)
-			if(allUsers[i].getUserName().equals(username)) return true;
+		for(int i = 0;i<allUsers.length-1;i++)
+			if(allUsers[i].getUserName().equals(username)) 
+				return true;
 		
 		return false;
 	}
 	
+	//Retrieves logged in User object from the DB
+	
+	public static User retrieveUserFromDb(String username)
+	{
+		String[] dummy1 = new String[5];
+		
+		String[] dummy2 = new String[5];
+		
+		int[] dummy3 = new int[4];
+		
+		//Placeholder code will not work without it.	
+		User user = new User("null","null",dummy3,dummy2,dummy1);
+		
+		for(int i = 0;i<allUsers.length-1;i++)
+			if(allUsers[i].getUserName().equals(username)) 
+				user = allUsers[i];
+		
+		return user;
+	}
+	
+	
+	//Validates password
+	public static boolean validatePassword(String username,String password)
+	{
+		
+		for(int i = 0;i<allUsers.length-1;i++)
+			
+			if(allUsers[i].getUserName().equals(username) && allUsers[i].getPassword().equals(password))
+				return true;
+				
+		return false;
+	}
+
+	//Prompts user for name
 	public static String promptUserName(Scanner input)
 	{
 		System.out.println("Type a username (no spaces)");
 		
 		String answer = input.next().toLowerCase();
-	
-		if(findUserInDb(answer))
+
+		if(isExistingUser(answer))
 		{
 			System.out.println("User name is already taken!");
+			
+			//If the username is taken then the funciton will run again
+			
 			promptUserName(input);
 		}
-		System.out.println(answer);
+			
 		return answer;
+	}
+	
+	//Prompts user for name when logging in
+	public static String promptUserNameLogin(Scanner input,int attempts)
+	{
+		//If the attempts to enter username exceeds 3 then the function will return an error
+		
+		if(attempts == 3) return "Error: You have exceeded the designated number of attempts.Please try again someother time";
+			
+		System.out.println("Type a username (no spaces)");
+		
+		String answer = input.next().toLowerCase();
+		
+		if(!isExistingUser(answer))
+		{
+			//If the user doesn't exsist then the function will run again
+			
+			System.out.println("Username doesn't exist try again");
+			
+			promptUserNameLogin(input,attempts++);
+
+		}
+		
+		//returns username
+		
+		return answer;
+	}
+	
+	//Prompts new user for password
+	public static String promptPassword(Scanner input)
+	{
+		System.out.println("Type a password, (no spaces)");
+		
+		String password = input.next().toLowerCase();
+		
+		return password;
 		
 	}
 	
+	
+	//Prompts existing user for password on login
+	
+	public static String promptPasswordLogin(Scanner input,String username)
+	{
+		System.out.println("Type a password, (no spaces)");
+		
+		String password = input.next().toLowerCase();
+		
+		if(!validatePassword(username,password)) {
+			
+			//If the password is not valid then the function will return an error message
+			
+			return "Error: Bad email password combination.";
+		}
+			
+		return password;
+		
+	}
+	
+	//Prompts the user for their favorite genre. The genres are listed 1-4 in the data base.
+	
+	public static int[] promptGenre(Scanner input)
+	{
+		System.out.println("Please type choose your favorite genre's from below, if you would like to select all please type 'all', if not just choose by number(please place spaces between numbers)");
+		
+		System.out.println("1)Action 2)Romance 3)Horror 4)Childrens");
+		
+		String genre = input.next();
+		
+		int[]genres;
+		
+		//If the user selects all genres then the array will fill with numbers from 1-4
+		if(genre.equals("all")) genres = new int[] {1,2,3,4};
+		
+		else {
+			Scanner line = new Scanner(genre);
+			
+			int num = line.nextInt();
+			
+			genres = numToArray(num);
+	
+			//Sorting array after numToArray function to get numbers in order
+			Arrays.sort(genres);
+			
+		}
+		
+		return genres;
+	}
 }
 
--- User.java
+++ User.java
@@ -34,4 +34,9 @@ public class User {
 	{
 		return username;
 	}
+	
+	public String getPassword()
+	{
+		return password;
+	}
 }
